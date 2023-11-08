269bae5ab5bf2f010947634e690713b7390faaa2
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-11-08 03:15:17 +0100

    project-dark-mode-React.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
+const card = [
+  {
+    title: 'Magnam Facilis Autem',
+    descript: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'
+  },
+  {
+    title: 'Magnam Facilis Autem',
+    descript: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'
+  },
+  {
+    title: 'Magnam Facilis Autem',
+    descript: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'
+  },
+  {
+    title: 'Magnam Facilis Autem',
+    descript: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'
+  },
+  {
+    title: 'Magnam Facilis Autem',
+    descript: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'
+  },
+  {
+    title: 'Magnam Facilis Autem',
+    descript: 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'
+  },
+    
+]
+console.log(card)
+
 
 function App() {
+
+  let [title, descript] = card
+
+  let [toggle, setToggle] = useState()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={toggle ? ('bg-dark trans') : ('trans')}>
+      
+      <div className="container  text-center">
+        <button onClick={()=>setToggle(!toggle)} className="btn btn-primary mt-5">DARK MODE</button>
+        <div className="row d-flex justify-content-center">
+            <>
+              { 
+                card.map((el)=>{
+                  let [title, descript] = card
+                  let style = 'm-5 p-5  shadow'
+                  return (
+                    <div className="col-12 col-sm-12 col-lg-6">
+                      <div className={`m-5 p-5  shadow ${toggle ? ('text-light opa') : ('')}` } >
+                        <h3>{el.title}</h3>
+                        <p>{el.descript}</p>
+                      </div>
+                      
+                    </div>
+                  )
+                
+                })
+              }
+            
+            </>
+            
+
+        </div>
+       
+
+      </div>
+      
+      
     </div>
   );
 }
+
+
+function Cards() {
+ 
+  return(
+    <>
+      { 
+        card.map((el)=>{
+           let [title, descript] = card
+          return (
+            <div className="col-6">
+              <div className="m-5 p-5  shadow">
+                <h3>{el.title}</h3>
+                <p>{el.descript}</p>
+              </div>
+              
+            </div>
+          )
+        
+        })
+      }
+    
+    </>
+   
+  ) 
+ 
+  
+}
+
+
 
 export default App;
